6a0ce828e3dbf830072d706ad11887cfc17d7239
aadit <pra.aadit@gmail.com>
2026-05-26 01:34:37 +0530

    new  pattern

---
+++ Factorialc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int number = 5 ;
+    int fact = 1;
+  for(int i =1;i<=number;i++){
+  fact = fact*i;
+  }
+  printf("%d",fact);
+    return 0;
+}
